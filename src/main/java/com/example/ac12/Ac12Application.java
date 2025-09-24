a949514859fc4518687f05a74a44f7e6eaafc324
GabrielRosa835 <gabriel.rosa.080305@gmail.com>
2025-09-24 19:14:08 -0300

    Project setup

---
+++ src/main/java/com/example/ac12/Ac12Application.java
@@ -0,0 +1,13 @@
+package com.example.ac12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ac12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ac12Application.class, args);
+	}
+
+}
